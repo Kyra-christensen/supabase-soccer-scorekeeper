391d49d25fcf45f933b7160ac17ba6c4ae669f9c
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-15 19:59:13 -0800

    working tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,32 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderGame, renderTeam } from '../render-utils.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
+test('renderGame should return a DOM element', (expect) => {
+    //Arrangeconst 
+    let currentGame = {
+        name1: 'kyra',
+        score1: 10,
+        name2: 'adam',
+        score2: 9
+    };
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div class="game"><div class="team"><p class="name">kyra</p><p class="score">10</p></div><div class="team"><p class="name">adam</p><p class="score">9</p></div></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderGame(currentGame);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('renderTeam should return a DOM element', (expect) => {
+    // eslint-disable-next-line no-useless-escape
+    const expected = '<div class="team"><p class="name"></p><p class="score"></p></div>';
+    const actual = renderTeam();
+
+    expect.equal(actual.outerHTML, expected);
 });
